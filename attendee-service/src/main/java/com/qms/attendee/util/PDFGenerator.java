5aee5d234200ad6080e06ebd00652b6574327743
Rahul Dubey <rahuldubey>
2023-01-16 18:44:04 +0530

    WIP

--- attendee-service/src/main/java/com/qms/attendee/util/PDFGenerator.java
+++ attendee-service/src/main/java/com/qms/attendee/util/PDFGenerator.java
@@ -1,5 +1,12 @@
 package com.qms.attendee.util;
 
-public class PDFGenerator {
+import org.springframework.stereotype.Component;
 
+@Component
+public class PDFGenerator {
+	https://springhow.com/spring-boot-pdf-generation/
+		https://zetcode.com/springboot/servepdf/
+			https://springjava.com/spring-boot/export-data-into-pdf-file-in-spring-boot
+				https://www.codejava.net/frameworks/spring-boot/pdf-export-example
+					https://javatechonline.com/generating-dynamic-pdf-report-using-spring-boot/
 }
